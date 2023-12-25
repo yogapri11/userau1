cc855eb2b22460283ad89594392d8e2e9961ef38
yogapri11 <154755595+yogapri11@users.noreply.github.com>
2023-12-25 18:33:34 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Home.css";
+import {Container,Button} from "react-bootstrap";
+
+const Home = () => {
+    return(
+        <Container>
+      <h1>Welcome to our website</h1>
+      <p>We are here to serve you</p>
+      <Button variant="primary" type="submit">Let's Get started</Button>
+    </Container>
+    
+    )
+}
+export default Home;
